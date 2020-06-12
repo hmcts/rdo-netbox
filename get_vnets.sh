8aa55a8f1325369b060fbed256587cf7a653361b
Daniel <daniel@alvestechnology.io>
2020-06-12 18:08:37 +0100

    debug

--- get_vnets.sh
+++ get_vnets.sh
@@ -1,10 +1,12 @@
-az login --service-principal -u $servicePrincipalId --password=$servicePrincipalKey --tenant $AZURE_TENANT_ID
-touch address_prefixes.json
+echo $AZURE_TENANT_ID
 
-for i in `az account list -o tsv | grep 531ff96d | awk '{print$6}'`; \
-do az account set --subscription $i; \
-az network vnet list --query '[].{Name:name, Location:location, ResourceGroup:resourceGroup, Network:addressSpace.addressPrefixes}' -o json >$i.json; \
-jq '.[] += {"ID": "'$i'"}' $i.json > $i.tmp && mv $i.tmp $i.json -f; \
-echo $(jq -s '[.[][]]' $i.json address_prefixes.json) >| address_prefixes.json; \
-rm $i.json; \
-done
+# az login --service-principal -u $servicePrincipalId --password=$servicePrincipalKey --tenant $AZURE_TENANT_ID
+# touch address_prefixes.json
+
+# for i in `az account list -o tsv | grep 531ff96d | awk '{print$6}'`; \
+# do az account set --subscription $i; \
+# az network vnet list --query '[].{Name:name, Location:location, ResourceGroup:resourceGroup, Network:addressSpace.addressPrefixes}' -o json >$i.json; \
+# jq '.[] += {"ID": "'$i'"}' $i.json > $i.tmp && mv $i.tmp $i.json -f; \
+# echo $(jq -s '[.[][]]' $i.json address_prefixes.json) >| address_prefixes.json; \
+# rm $i.json; \
+# done

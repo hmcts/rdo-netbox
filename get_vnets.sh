d011bc169bcaecf55fc3ad200d0418e040ba839c
Daniel <daniel@alvestechnology.io>
2020-06-12 17:36:50 +0100

    debug

--- get_vnets.sh
+++ get_vnets.sh
@@ -1,10 +1,8 @@
-az account list -o tsv --all | awk '{print$6}' 
-
-# touch address_prefixes.json
-# for i in `az account list -o tsv | grep 531ff96d | awk '{print$6}'`; \
-# do az account set --subscription $i; \
-# az network vnet list --query '[].{Name:name, Location:location, ResourceGroup:resourceGroup, Network:addressSpace.addressPrefixes}' -o json >$i.json; \
-# jq '.[] += {"ID": "'$i'"}' $i.json > $i.tmp && mv $i.tmp $i.json -f; \
-# echo $(jq -s '[.[][]]' $i.json address_prefixes.json) >| address_prefixes.json; \
-# rm $i.json; \
-# done
+touch address_prefixes.json
+for i in `az account list -o tsv | grep 531ff96d | awk '{print$6}'`; \
+do az account set --subscription $i; \
+az network vnet list --query '[].{Name:name, Location:location, ResourceGroup:resourceGroup, Network:addressSpace.addressPrefixes}' -o json >$i.json; \
+jq '.[] += {"ID": "'$i'"}' $i.json > $i.tmp && mv $i.tmp $i.json -f; \
+echo $(jq -s '[.[][]]' $i.json address_prefixes.json) >| address_prefixes.json; \
+rm $i.json; \
+done

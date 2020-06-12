a0b2ed20be4c4702596e7917087b9dc09b6d149c
Daniel <daniel@alvestechnology.io>
2020-06-12 17:51:58 +0100

    debug

--- get_vnets.sh
+++ get_vnets.sh
@@ -1,4 +1,7 @@
+echo $servicePrincipalId
+
 touch address_prefixes.json
+
 for i in `az account list -o tsv | grep 531ff96d | awk '{print$6}'`; \
 do az account set --subscription $i; \
 az network vnet list --query '[].{Name:name, Location:location, ResourceGroup:resourceGroup, Network:addressSpace.addressPrefixes}' -o json >$i.json; \

93b53c675c3424b33e9fb52cfcc1f1f91c3164fa
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-08 08:11:36 +0100

    update

--- components/netbox/init.tf
+++ components/netbox/init.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=1.4.6"
   backend "azurerm" {
-    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+    /* subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263" */
   }
   required_providers {
     azurerm = {

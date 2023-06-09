9bb67b23a0d9f896576c5663a33aa39efb5fa7d2
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-09 10:20:00 +0100

    update

--- components/netbox/redis.tf
+++ components/netbox/redis.tf
@@ -12,3 +12,20 @@ module "redis" {
   private_endpoint_enabled      = true
   public_network_access_enabled = false
 }
+
+resource "azurerm_key_vault_secret" "redis_hostname" {
+  name         = "netbox-redis-hostname"
+  value        = module.redis.host_name
+  key_vault_id = azurerm_key_vault.netbox_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "redis_accesskey" {
+  name         = "netbox-redis-accesskey"
+  value        = module.redis.access_key
+  key_vault_id = azurerm_key_vault.netbox_key_vault.id
+}
+resource "azurerm_key_vault_secret" "redis_port" {
+  name         = "netbox-redis-port"
+  value        = module.redis.redis_port
+  key_vault_id = azurerm_key_vault.netbox_key_vault.id
+}

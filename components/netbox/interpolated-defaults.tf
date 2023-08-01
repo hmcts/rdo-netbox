18b7c33d0c4dec14cffc84afbf9918ea55334740
Khaled Siddat <khaled.siddat@justice.gov.uk>
2023-08-01 14:07:49 +0100

    DTSPO-14613 adding expires after

--- components/netbox/interpolated-defaults.tf
+++ components/netbox/interpolated-defaults.tf
@@ -3,8 +3,9 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 module "tags" {
-  source      = "github.com/hmcts/terraform-module-common-tags?ref=master"
-  builtFrom   = var.builtFrom
-  environment = var.environment
-  product     = var.product
+  source       = "github.com/hmcts/terraform-module-common-tags?ref=master"
+  builtFrom    = var.builtFrom
+  environment  = var.environment
+  product      = var.product
+  expiresAfter = "3000-01-01"
 }
